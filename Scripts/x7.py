0976bbe53f345726fa1226a5cae048bf4a67549c
Entropy <nessisspy@yahoo.gr>
2022-03-16 01:55:21 +0200

    alg fix

--- Scripts/x7.py
+++ Scripts/x7.py
@@ -17,7 +17,7 @@ class ExampleDisplacedAnalysis(Module): #this just turned out to be a better opt
 
     def beginJob(self, histFile=None, histDirName=None):
         Module.beginJob(self, histFile, histDirName)
-        # GENERAL 
+        # GENERAL
         # CHARGINOS
         self.h_chpt = ROOT.TH1F('chpt', 'Chargino Transverse Momentum', 250, 0, 1100)
         self.h_cheta = ROOT.TH1F('cheta', 'Chargino Pseudorapidity', 250, -6, 6)
@@ -33,29 +33,30 @@ class ExampleDisplacedAnalysis(Module): #this just turned out to be a better opt
         finalSampleEvent = []
         feventSum = ROOT.TLorentzVector()
         i = 1 #for logging
-        
+
         for particle in genParts:
             if abs(particle.pdgId) in finalReq: #particle is muon
                 mother = genParts[particle.genPartIdxMother] if particle.genPartIdxMother in range(len(genParts)) else None # to be W
                 if mother is not None:
                     grandmother = genParts[mother.genPartIdxMother] if mother.genPartIdxMother in range(len(genParts)) else None # to be chargino
-                    if grandmother is not None:   
+                    if grandmother is not None:
                         grandgrandmother = genParts[grandmother.genPartIdxMother] if grandmother.genPartIdxMother in range(len(genParts)) else None # to be None, popped from protons
                         gg3 = genParts[grandgrandmother.genPartIdxMother] if grandgrandmother.genPartIdxMother in range(len(genParts)) else None # to be None, popped from protons
-                        if gg3 is None:
-                            print("gg3 is None!)"
-                        if gg3 is not None:
-                            print("gg3 id: " + str(gg3.pdgId))
-                        #if 3==3: #adj
-                        #    if abs(particle.pdgId) ==  13 and abs(mother.pdgId) == 24 and abs(grandmother.pdgId) == 1000024: 
-		        #        finalSampleEvent.append(grandgrandmother)
-		        #        self.h_chpt.Fill(grandgrandmother.pt)
-	    	         #       self.h_cheta.Fill(grandgrandmother.eta)
-	    	         #       self.h_chphi.Fill(grandgrandmother.phi)
-		         #       #tedious logging to see if things are ok
-		         #       print("id: " + str(particle.pdgId) + ", mid: " + str(mother.pdgId) + ", gmid: " + str(grandmother.pdgId) + ", ggmid: " + str(grandgrandmother.pdgId) + ", loopnum: " + str(i))
-		        #        i += 1 
-	print("finalSampleEvent size: " + str(len(finalSampleEvent)))     
+                        #if gg3 is None:
+                        #    print("gg3 is None!)"
+                        #if gg3 is not None:
+                        #    print("gg3 id: " + str(gg3.pdgId))
+                        if 3==3: #adj
+                            if abs(particle.pdgId) ==  13 and abs(mother.pdgId) == 24 and abs(grandmother.pdgId) == 1000024:
+    		                    finalSampleEvent.append(grandgrandmother)
+    		                    self.h_chpt.Fill(grandgrandmother.pt)
+    	    	                self.h_cheta.Fill(grandgrandmother.eta)
+    	    	                self.h_chphi.Fill(grandgrandmother.phi)
+		               #tedious logging to see if things are ok
+		                        print("id: " + str(particle.pdgId) + ", mid: " + str(mother.pdgId) + ", gmid: " + str(grandmother.pdgId) + ", ggmid: " + str(grandgrandmother.pdgId) + ", loopnum: " + str(i))
+                                print("gg3id: " + str(gg3.pdgId))
+		                        i += 1
+	print("finalSampleEvent size: " + str(len(finalSampleEvent)))
 	#if len(finalSampleEvent) == 2:
 	#    i = 1
 	#    if finalSampleEvent[0].pdgId == -finalSampleEvent[1].pdgId:
@@ -67,7 +68,7 @@ class ExampleDisplacedAnalysis(Module): #this just turned out to be a better opt
 	#    	    print("We got one! particle sample num: " + str(i) + " out of 2")
 	#    	    feventSum += particle.p4()
 	#    	    i += 1
-	   	     
+
         return True
 
     def endJob(self):
@@ -80,7 +81,7 @@ class ExampleDisplacedAnalysis(Module): #this just turned out to be a better opt
              save = "x7/h_" + hist.GetName() + ".png"
              self.c.SaveAs(save)
         Module.endJob(self)
-        
+
 preselection = "" ## no preselection
 files = ["{}/src/DisplacedCharginos_Dec8_2DispMuonsSkim/SMS_TChiWW_Disp_200_180_10_final.root".format(os.environ['CMSSW_BASE'])]
 p = PostProcessor(".", files, cut=preselection, branchsel=None, modules=[ExampleDisplacedAnalysis()], noOut=True, histFileName="x6.root", histDirName="plots")
