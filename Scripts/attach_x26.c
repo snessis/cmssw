518ae09f0c38dc45f7942b22204431ce1701d64a
Entropy <nessisspy@yahoo.gr>
2022-05-24 21:57:59 +0300

    intro

---
+++ Scripts/attach_x26.c
@@ -0,0 +1,29 @@
+#include "TCanvas.h"
+#include "TROOT.h"
+#include "TF1.h"
+#include "TLegend.h"
+#include "TArrow.h"
+#include "TLatex.h"
+
+void attach_x26() {
+//    gROOT->SetStyle("Plain");   // set plain TStyle
+  //  gStyle->SetOptStat(111111); // draw statistics on plots,
+                            // (0) for no output
+//    gStyle->SetOptFit(1111);    // draw fit results on plot,
+                            // (0) for no ouput
+    //gStyle->SetPalette(57);     // set color map
+  //  gStyle->SetOptTitle(0);     // suppress title box
+
+    //TCanvas c("canv", "The Canvas (post-analysis)",700, 1000); // create a canvas, specify position and size in pixels
+
+TFile *f1 = new TFile("{}/src/gitRepo/x26_1/x26_1_jetht.root");
+TFile *f2 = new TFile("{}/src/gitRepo/x26_1/x26_1_jetht.root");
+f1.ls();
+f2.ls();
+
+//TH1F * h1 = (TH1F*)f1.Get(“jetht”);
+//TH1F * h2 = (TH1F*)f2.Get(“jetht”);
+//TH1F * h3 = *h1 + *h2;
+
+//h1->Draw();
+}
