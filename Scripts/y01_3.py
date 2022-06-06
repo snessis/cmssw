b9de2056a6c3a2059f45ee646a2f8a1b53270baf
Entropy <nessisspy@yahoo.gr>
2022-06-07 00:07:40 +0300

    qfix

--- Scripts/y01_3.py
+++ Scripts/y01_3.py
@@ -276,5 +276,5 @@ preselection = "Jet_pt >= 30 && MET_pt >=100"
 #preselection = ""
 #files = ["{}/src/DisplacedCharginos_May4_unskimmed/SMS_TChiWW_Disp_200_195_2.root".format(os.environ['CMSSW_BASE'])]
 files = (["{}/src/displacedSOS_mainbkg_260422_nanoV7/WJetsToLNu_HT400to600.root".format(os.environ['CMSSW_BASE'])])
-p = PostProcessor(".", files, cut=preselection, branchsel=None, modules=[ExampleDisplacedAnalysis()], noOut=True, histFileName="x" + ver + ".root", histDirName="plots")
+p = PostProcessor(".", files, cut=preselection, branchsel=None, modules=[ExampleDisplacedAnalysis()], noOut=True, histFileName="y" + ver + ".root", histDirName="plots")
 p.run()
