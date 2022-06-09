8200699b8cebde0b0fc266684b447d9b803be6e1
Entropy <nessisspy@yahoo.gr>
2022-06-09 03:09:06 +0300

    qfix

--- Scripts/attach_y02.c
+++ Scripts/attach_y02.c
@@ -22,7 +22,7 @@ void attach_y02() {
   double XSEC6 = 1.071*1.17*1000;
   double XSEC7 = 0.00819*1.17*1000;
   double XSECCH = 0.902569*1000;
-  double L = 60;
+  double L = 60*10000;
   double SF1 = N1 / 29521158;
   double SF2 = N2 / 25468933;
   double SF3 = N3 / 5932701;
