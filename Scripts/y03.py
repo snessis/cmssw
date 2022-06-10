666b60c2ed0f43d681a144c19c98e4a36a6b6de0
Entropy <nessisspy@yahoo.gr>
2022-06-11 01:45:17 +0300

    qfix

--- Scripts/y03.py
+++ Scripts/y03.py
@@ -453,7 +453,7 @@ class ExampleDisplacedAnalysis(Module):
         self.leg_pt_sus.SetMargin(0.15)
         self.leg_pt_sus.AddEntry(self.h_chpt, "#tilde{#chi}_{1}^{#pm}", "L" )
         self.leg_pt_sus.AddEntry(self.h_neupt, "#tilde{#chi}_{1}^{0}", "L")
-        self.s_pt_sm.Draw()
+        self.s_pt_sus.Draw()
         self.leg_pt_sus.Draw()
         self.s_pt_sus.GetXaxis().SetTitle("p_t \\mbox{ (GeV)}")
         self.s_pt_sus.GetYaxis().SetTitle("Counts")
