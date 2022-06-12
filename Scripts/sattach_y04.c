50a29d7b00149a8302ef368cf6b5afeb0294ee11
Entropy <nessisspy@yahoo.gr>
2022-06-13 00:57:40 +0300

    qfix

--- Scripts/sattach_y04.c
+++ Scripts/sattach_y04.c
@@ -61,6 +61,9 @@ void sattach_y04() {
   TH1F *met5 = (TH1F*)f5->Get("plots/metpt");
   TH1F *met6 = (TH1F*)f6->Get("plots/metpt");
   TH1F *met7 = (TH1F*)f7->Get("plots/metpt");
+  TH1F *met8 = (TH1F*)f8->Get("plots/metpt");
+  TH1F *met9 = (TH1F*)f9->Get("plots/metpt");
+  TH1F *met10 = (TH1F*)f10->Get("plots/metpt");
   TH1F *metch = (TH1F*)fch->Get("plots/metpt");
   TH1F *metbg = new TH1F("bg_met", "\\mbox{MET (background)}", 90, 0, 400);
 
@@ -71,6 +74,9 @@ void sattach_y04() {
   TH1F *h5_1 = (TH1F*)f5->Get("plots/jetht1");
   TH1F *h6_1 = (TH1F*)f6->Get("plots/jetht1");
   TH1F *h7_1 = (TH1F*)f7->Get("plots/jetht1");
+  TH1F *h8_1 = (TH1F*)f8->Get("plots/jetht1");
+  TH1F *h9_1 = (TH1F*)f9->Get("plots/jetht1");
+  TH1F *h10_1 = (TH1F*)f10->Get("plots/jetht1");
   TH1F *hch_1 = (TH1F*)fch->Get("plots/jetht1");
   TH1F *hbg_1 = new TH1F("bg_jetht1", "\\mbox{Total Jet HT (background) for length cut } d_1", 90, 0, 3500);
   TH1F *htotal_1 = new TH1F("total_jetht1", "\\mbox{Total Jet HT (plus chargino) for length cut } d_1", 90, 0, 3500);
@@ -82,6 +88,9 @@ void sattach_y04() {
   TH1F *h5_2 = (TH1F*)f5->Get("plots/jetht2");
   TH1F *h6_2 = (TH1F*)f6->Get("plots/jetht2");
   TH1F *h7_2 = (TH1F*)f7->Get("plots/jetht2");
+  TH1F *h8_2 = (TH1F*)f8->Get("plots/jetht2");
+  TH1F *h9_2 = (TH1F*)f9->Get("plots/jetht2");
+  TH1F *h10_2 = (TH1F*)f10->Get("plots/jetht2");
   TH1F *hch_2 = (TH1F*)fch->Get("plots/jetht2");
   TH1F *hbg_2 = new TH1F("bg_jetht2", "\\mbox{Total Jet HT (background) for length cut } d_2", 90, 0, 3500);
   TH1F *htotal_2 = new TH1F("total_jetht2", "\\mbox{Total Jet HT (plus chargino) for length cut } d_2", 90, 0, 3500);
@@ -93,6 +102,9 @@ void sattach_y04() {
   TH1F *h5_3 = (TH1F*)f5->Get("plots/jetht3");
   TH1F *h6_3 = (TH1F*)f6->Get("plots/jetht3");
   TH1F *h7_3 = (TH1F*)f7->Get("plots/jetht3");
+  TH1F *h8_3 = (TH1F*)f8->Get("plots/jetht3");
+  TH1F *h9_3 = (TH1F*)f9->Get("plots/jetht3");
+  TH1F *h10_3 = (TH1F*)f10->Get("plots/jetht3");
   TH1F *hch_3 = (TH1F*)fch->Get("plots/jetht3");
   TH1F *hbg_3 = new TH1F("bg_jetht3", "\\mbox{Total Jet HT (background) for length cut } d_3", 90, 0, 3500);
   TH1F *htotal_3 = new TH1F("total_jetht3", "\\mbox{Total Jet HT (plus chargino) for length cut } d_3", 90, 0, 3500);
@@ -104,6 +116,9 @@ void sattach_y04() {
   TH1F *h5_4 = (TH1F*)f5->Get("plots/jetht4");
   TH1F *h6_4 = (TH1F*)f6->Get("plots/jetht4");
   TH1F *h7_4 = (TH1F*)f7->Get("plots/jetht4");
+  TH1F *h8_4 = (TH1F*)f8->Get("plots/jetht4");
+  TH1F *h9_4 = (TH1F*)f9->Get("plots/jetht4");
+  TH1F *h10_4 = (TH1F*)f10->Get("plots/jetht4");
   TH1F *hch_4 = (TH1F*)fch->Get("plots/jetht4");
   TH1F *hbg_4 = new TH1F("bg_jetht4", "\\mbox{Total Jet HT (background) for length cut } d_4", 90, 0, 3500);
   TH1F *htotal_4 = new TH1F("total_jetht4", "\\mbox{Total Jet HT (plus chargino) for length cut } d_4", 90, 0, 3500);
@@ -115,6 +130,9 @@ void sattach_y04() {
   TH1F *h5_5 = (TH1F*)f5->Get("plots/jetht5");
   TH1F *h6_5 = (TH1F*)f6->Get("plots/jetht5");
   TH1F *h7_5 = (TH1F*)f7->Get("plots/jetht5");
+  TH1F *h8_5 = (TH1F*)f8->Get("plots/jetht5");
+  TH1F *h9_5 = (TH1F*)f9->Get("plots/jetht5");
+  TH1F *h10_5 = (TH1F*)f10->Get("plots/jetht5");
   TH1F *hch_5 = (TH1F*)fch->Get("plots/jetht5");
   TH1F *hbg_5 = new TH1F("bg_jetht5", "\\mbox{Total Jet HT (background) for length cut } d_5", 90, 0, 3500);
   TH1F *htotal_5 = new TH1F("total_jetht5", "\\mbox{Total Jet HT (plus chargino) for length cut } d_5", 90, 0, 3500);
