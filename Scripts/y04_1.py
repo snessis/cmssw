b621e3fcd10c55933a6dac5c39645e13e21c0a42
Entropy <nessisspy@yahoo.gr>
2022-06-12 22:18:41 +0300

    qfix

--- Scripts/y04_1.py
+++ Scripts/y04_1.py
@@ -242,6 +242,7 @@ class ExampleDisplacedAnalysis(Module):
         if len(mus) == 0:
             return False
         for Muon in Muons:
+            print(Muon.dz)
             if genParts[Muon.genPartIdx] in mus:
                 d = math.sqrt(math.pow(Muon.dxy, 2) + math.pow(Muon.dz, 2))
                 if Muon.pt >= 3 and abs(Muon.eta) <= 2.5 and METpt >= 100 and d >= 0.017:
