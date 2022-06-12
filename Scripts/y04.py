b956fbcf49f39adaa30f4d47b24dec2cfc6829d3
Entropy <nessisspy@yahoo.gr>
2022-06-12 21:54:00 +0300

    qfix

--- Scripts/y04.py
+++ Scripts/y04.py
@@ -243,6 +243,7 @@ class ExampleDisplacedAnalysis(Module):
             if abs(jet.pt) >= 30:
                 jets.append(jet)
         for Muon in Muons:
+            print(Muon.dz)
             if genParts[Muon.genPartIdx] in mus:
                 d = math.sqrt(math.pow(Muon.dxy, 2) + math.pow(Muon.dz, 2))
                 if Muon.pt >= 3 and abs(Muon.eta) <= 2.5 and METpt >= 100 and d >= 0.017:
