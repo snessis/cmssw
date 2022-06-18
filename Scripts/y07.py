db862735fc70b1c490a4fe33df801a32a7262a0b
Entropy <nessisspy@yahoo.gr>
2022-06-18 23:30:22 +0300

    qfix

--- Scripts/y07.py
+++ Scripts/y07.py
@@ -23,11 +23,11 @@ events_all = 556249
 locateFinalStates = [13, 14, 1000022]
 leptonic = [13, 14]
 hadronic = [1,2,3,4,5,6,21]
-d1 = 0.1
-d2 = 0.125
-d3 = 0.15
-d4 = 0.175
-d5 = 0.2
+d1 = 0.01
+d2 = 0.02
+d3 = 0.035
+d4 = 0.06
+d5 = 0.075
 class ExampleDisplacedAnalysis(Module):
     def __init__(self):
         self.writeHistFile = True
