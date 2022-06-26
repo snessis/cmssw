64e4ea1c0a4ea3464dd7cb37fcf0105a38e68bcf
Entropy <nessisspy@yahoo.gr>
2022-06-26 05:37:30 +0300

    qfix

--- Scripts/corr_d_eff.c
+++ Scripts/corr_d_eff.c
@@ -4,7 +4,7 @@
 #include "TLegend.h"
 #include "TLatex.h"
 double bg_fit(double *x, double *par) {
-	return 100 - par[0]*TMath::Exp(-abs(par[1])*x[0])
+	return 100 - par[0]*TMath::Exp(-abs(par[1])*x[0]);
 }
 
 void corr_d_eff() {
@@ -81,7 +81,7 @@ void corr_d_eff() {
   c_corr->GetFrame()->SetBorderMode(-1);
   c_corr->GetFrame()->SetBorderSize(5);
   TF1 *bg_fit_func = new TF1("bg_fit_func",bg_fit,0,0.8,2);
-  gr_bkg_tt->Fit("bg_fit_func")
+  gr_bkg_tt->Fit("bg_fit_func");
 
   gr_sig->Draw("AP");
   leg_sig->Draw();
